569214a060499badc72aacc7ae76b315a19b9786
Dots <dotvnl@gmail.com>
2024-05-13 17:46:31 +0300

    update js tiers on nopre

--- src/kz/mode/kz_mode_np.h
+++ src/kz/mode/kz_mode_np.h
@@ -59,12 +59,12 @@ class KZNoPreModeService : public KZModeService
 
 	f32 distanceTiers[JUMPTYPE_COUNT - 3][DISTANCETIER_COUNT] = {
 		{217.0f, 250.0f, 255.0f, 260.0f, 265.0f, 270.0f}, // LJ
-		{217.0f, 260.0f, 265.0f, 270.0f, 275.0f, 280.0f}, // BH
-		{217.0f, 262.0f, 267.0f, 272.0f, 277.0f, 282.0f}, // MBH
-		{217.0f, 258.0f, 263.0f, 268.0f, 273.0f, 278.0f}, // WJ
+		{217.0f, 275.0f, 280.0f, 285.0f, 290.0f, 295.0f}, // BH
+		{217.0f, 275.0f, 280.0f, 285.0f, 290.0f, 295.0f}, // MBH
+		{217.0f, 275.0f, 280.0f, 285.0f, 290.0f, 295.0f}, // WJ
 		{120.0f, 180.0f, 185.0f, 190.0f, 195.0f, 200.0f}, // LAJ
 		{217.0f, 260.0f, 265.0f, 270.0f, 275.0f, 280.0f}, // LAH
-		{217.0f, 260.0f, 265.0f, 270.0f, 275.0f, 280.0f}, // JB
+		{217.0f, 275.0f, 280.0f, 285.0f, 290.0f, 295.0f}, // JB
 	};
 
 	const char *modeCvarValues[KZ::mode::numCvar] = {
