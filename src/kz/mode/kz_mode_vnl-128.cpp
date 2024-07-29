562f32534257d9cde2ee74050f1a565bd4ac3adc
Dots <dotvnl@gmail.com>
2024-07-29 19:09:26 +0300

    fix compile

--- src/kz/mode/kz_mode_vnl-128.cpp
+++ src/kz/mode/kz_mode_vnl-128.cpp
@@ -337,7 +337,7 @@ void KZVanilla128ModeService::OnProcessMovement()
 		this->player->GetPlayerPawn()->m_flVelocityModifier(1.0f);
 	}
 	this->CheckVelocityQuantization();
-	this->RemoveCrouchJumpBind();
+	//this->RemoveCrouchJumpBind();
 	this->ReduceDuckSlowdown();
 	this->InterpolateViewAngles();
 	this->UpdateAngleHistory();
